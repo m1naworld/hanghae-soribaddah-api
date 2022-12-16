e59e6c6783d8a6d6009fd30b53a0015617fa3b5d
JeekLee <j3kkun@gmail.com>
2022-12-16 20:39:28 +0900

    feat: @EnableJPAAuditing 추가
    제목과 같음

--- src/main/java/com/sparta/soundsea/SoundseaApplication.java
+++ src/main/java/com/sparta/soundsea/SoundseaApplication.java
@@ -1,11 +1,13 @@
 package com.sparta.soundsea;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SoundseaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SoundseaApplication.class, args);
     }
