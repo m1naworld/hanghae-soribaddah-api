3dec78a2b2c6d4cb109479915863c6e4138123cf
mina <m1nna@naver.com>
2022-12-16 14:02:07 +0900

    init

---
+++ src/main/java/com/sparta/soundsea/SoundseaApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.soundsea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundseaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoundseaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/soundsea/SoundseaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.soundsea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundseaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
