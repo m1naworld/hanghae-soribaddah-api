56ae2e4d648dc5af8722476d263570ba5cc15699
jaekyeong1 <kingjk0157@naver.com>
2022-12-16 23:31:22 +0900

    feat: Comment API, Error Code 등록

--- src/main/java/com/sparta/soundsea/common/exception/ExceptionMessage.java
+++ src/main/java/com/sparta/soundsea/common/exception/ExceptionMessage.java
@@ -6,7 +6,17 @@ import lombok.Getter;
 public enum ExceptionMessage {
 
     // common
-    INTERNAL_SERVER_ERROR_MSG(500,"서버 에러입니다.");
+    INTERNAL_SERVER_ERROR_MSG(500,"서버 에러입니다."),
+
+    // 403 토큰 만료
+    EXPIRATION_TOKEN(403, "Access Token이 만료되었습니다"),
+
+    // 403 권한 없음
+    INVALID_AUTH_TOKEN(403,"권한이 없는 사용자 입니다"),
+
+    // 404 해당댓글 없음
+    COMMENT_NOT_FOUND(404, "해당 댓글을 찾을 수 없습니다.");
+
     private final int statusCode;
     private final String msg;
 
