bd7452fa96fede3b5e01f32e04396e3b9cc856a1
JeekLee <j3kkun@gmail.com>
2022-12-17 14:39:11 +0900

    feat: 로그인 관련 DTO 추가
    1. RequestSignUpUserDto
    - RequestBody의 내용을 받아오기 위한 DTO.
    - Controller에서
    SignUpUUserDto로 변환하여 Service 계층에 넘긴다.
    
    2. SignUpUserDto
    - 서비스 단에서 메소드에
    의해 유효성을 검증받는다.
    - 유효성 검사가 진행된 이후에는, Mapper를 통해 Entity로 변환된다.

---
+++ src/main/java/com/sparta/soundsea/user/dto/RequestSignUpUserDto.java
@@ -0,0 +1,24 @@
+package com.sparta.soundsea.user.dto;
+
+import lombok.Getter;
+
+@Getter
+public class RequestSignUpUserDto {
+    private String loginId;
+    private String nickname;
+    private String password;
+    private boolean social = false;
+    private boolean admin = false;
+    private String adminToken = "";
+
+    // Builder를 통해 RequestDTO를 ServiceDTO로 변경
+    public SignUpUserDto toSignUpUserDto() {
+        return SignUpUserDto.builder()
+                .loginId(loginId)
+                .nickname(nickname)
+                .password(password)
+                .social(social)
+                .admin(admin)
+                .build();
+    }
+}
