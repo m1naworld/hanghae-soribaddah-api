9a6058329980f9d6f5eb044cac7d61bcace34dba
JeekLee <j3kkun@gmail.com>
2022-12-17 14:40:53 +0900

    feat: UserMapper 구현
    UserMapper는 SignUpUserDto를 바탕으로 User Entity를 생성한다.
    
    생성하는 과정은 다음과 같다.
    
    1.
    SignUpUserDto의 AdminToken 검증
    2. Password 암호화 (passwordEncoder)
    3. Entity
    생성

---
+++ src/main/java/com/sparta/soundsea/user/mapper/UserMapper.java
@@ -0,0 +1,49 @@
+package com.sparta.soundsea.user.mapper;
+
+import com.sparta.soundsea.user.dto.SignUpUserDto;
+import com.sparta.soundsea.user.entity.User;
+import com.sparta.soundsea.user.entity.UserRole;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import static com.sparta.soundsea.common.exception.ExceptionMessage.ADMIN_TOKEN_MISMATCH_ERROR_MSG;
+
+@Component
+@PropertySource("classpath:security.properties")
+@RequiredArgsConstructor
+public class UserMapper {
+    @Value("${admin.token}")
+    String ADMIN_TOKEN;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public User toEntity(SignUpUserDto signUpUserDto) throws IllegalArgumentException{
+        UserRole role = UserRole.USER;
+
+        // 1. ADMIN_TOKEN과 일치하는지 확인
+        if (signUpUserDto.isAdmin()) {
+            if(!signUpUserDto.getAdminToken().equals(ADMIN_TOKEN)) {
+                // 에러가 정상적으로 터지는지 확인 필요
+                throw new IllegalArgumentException(ADMIN_TOKEN_MISMATCH_ERROR_MSG.getMsg());
+            }
+            // 2. 일치할 경우 ADMIN으로 권한 설정
+            role = UserRole.ADMIN;
+        }
+
+        // 2. Password 암호화
+        String encryptedPassword = passwordEncoder.encode(signUpUserDto.getPassword());
+
+        // 3. Entity 생성
+        return User.builder()
+                .loginId(signUpUserDto.getLoginId())
+                .nickname(signUpUserDto.getNickname())
+                .password(encryptedPassword)
+                .social(signUpUserDto.isSocial())
+                .role(role)
+                .build();
+    }
+}
