6782ab0b119e950ac28c1ac06539be23dacea9b9
jaekyeong1 <kingjk0157@naver.com>
2022-12-18 16:58:37 +0900

    feat: 댓글 controller,dto 설정

--- src/main/java/com/sparta/soundsea/comment/dto/CommentResponseDto.java
+++ src/main/java/com/sparta/soundsea/comment/dto/CommentResponseDto.java
@@ -1,16 +1,34 @@
 package com.sparta.soundsea.comment.dto;
 
+import com.sparta.soundsea.comment.entity.Comment;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
 public class CommentResponseDto {
 
+    private Long commentId;
 
-    public CommentResponseDto(String statusCode, String msg){
+    private String loginId;
+
+    private String contents;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime lastModifiedAt;
+
+    public CommentResponseDto(Comment comment){
+
+        this.commentId = comment.getId();
+        this.loginId = comment.getUser().getLoginId();
+        this.contents = comment.getContents();
+        this.createdAt = comment.getCreatedAt();
+        this.lastModifiedAt = comment.getLastModifiedAt();
+
 
 
     }
-
 }
--- src/main/java/com/sparta/soundsea/common/response/ResponseMessage.java
+++ src/main/java/com/sparta/soundsea/common/response/ResponseMessage.java
@@ -6,7 +6,14 @@ import lombok.Getter;
 public enum ResponseMessage {
     // User
     SIGNUP_USER_SUCCESS_MSG(200, "회원 가입 성공"),
-    LOGIN_USER_SUCCESS_MSG(200, "유저 로그인 성공");
+    LOGIN_USER_SUCCESS_MSG(200, "유저 로그인 성공"),
+
+    COMMENT_CREATE_SUCCESS_MSG(200,"댓글 작성 성공"),
+
+    COMMENT_UPDATE_SUCCESS_MSG(200, "댓글 수정 성공"),
+
+
+    COMMENT_DELETE_SUCCESS_MSG(200, "댓글 삭제 성공");
 
     private final int statusCode;
     private final String msg;
