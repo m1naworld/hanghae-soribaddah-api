b54e4a1c21df984c3c6af0c7ee882b2b44246d58
JeekLee <j3kkun@gmail.com>
2022-12-20 13:57:51 +0900

    fix: 로그인 하지 않은 유저에게 인증/인가 적용
    - 로그인 하지 않은 유저에게 임시로 인증/인가 부여
    - 권한은 Foreign으로 사용

--- src/main/java/com/sparta/soundsea/security/CustomUserDetailsService.java
+++ src/main/java/com/sparta/soundsea/security/CustomUserDetailsService.java
@@ -1,6 +1,7 @@
 package com.sparta.soundsea.security;
 
 import com.sparta.soundsea.user.entity.User;
+import com.sparta.soundsea.user.entity.UserRole;
 import com.sparta.soundsea.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,6 +17,12 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
         System.out.println("UserDetailsServiceImpl.loadUserByUsername : " + userName);
+        // 1. UserName이 Foreign인 경우(=로그인하지 않은 사용자인 경우 = 토큰이 비어있는 경우)
+        if (userName.equals("Foreign")) {
+            // 2. 임시로 사용할 User Foreign 생성 및 반환
+            User foreign = new User(userName, null, null, false, UserRole.FOREIGN);
+            return new CustomUserDetails(foreign, foreign.getLoginId(), foreign.getPassword());
+        }
 
         // 사용자 조회
         User user = userRepository.findByLoginId(userName)
